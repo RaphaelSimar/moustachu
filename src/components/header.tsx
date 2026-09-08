dd4f3d60afdd64a5e2b9216c012fd358a803e15e
Raphaël <simar.raphael@laposte.net>
2026-09-08 23:58:45 +0200

    feat: more responsiveness

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,15 +6,14 @@ import logo from "../assets/logo/moustachu_logo.svg";
 export function Header() {
   return (
     <div className="bg-white">
-      <header className="bg-primary-500 flex items-center justify-between items-end h-[100px] p-10 pb-4 pl-50 overflow-y">
+      <header className="bg-primary-500 flex items-center justify-between items-end h-[100px] p-10 pb-4 pl-33 md:pl-50 overflow-y">
         <div className="flex items-center justify-between gap-4 w-full items-end">
           <Link href="/" className="absolute left-5 translate-y-1/2">
             <Image
               src={logo}
               alt="Moustachu"
-              width={130}
               priority
-              className="transition-all duration-300 hover:scale-105 hover:drop-shadow-lg"
+              className="w-20 md:w-32 transition-all duration-300 hover:scale-105 hover:drop-shadow-lg"
             />
           </Link>
 
@@ -49,11 +48,7 @@ export function Header() {
             href="/"
             className="group flex items-center gap-2 text-secondary-500 md:text-xl font-bold tracking-tight"
           >
-            <IconTipJarEuro
-              size={50}
-              stroke={2}
-              className="transition-all duration-300 hover:scale-120 drop-shadow-lg duration-300 group-hover:rotate-10"
-            />
+            <IconTipJarEuro className="h-8 w-8 transition-all duration-300 drop-shadow-lg group-hover:rotate-10 group-hover:scale-120 md:h-[45px] md:w-[45px]" />
           </Link>
         </div>
       </header>
